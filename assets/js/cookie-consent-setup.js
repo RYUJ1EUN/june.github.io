d23593d0ae33b25ed6ce75b15b088f762688732b
RYUJ1EUN <J.ryu.jieun@gmail.com>
2026-02-11 04:34:39 +0000

    Deploying to gh-pages from @ RYUJ1EUN/june.github.io@6616a737664cd15e92674a028d5b6ea3958a5b11 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://RYUJ1EUN.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://ryuj1eun.github.io/june.github.io/#contact">contact us</a>.'
               }
             ]
           }
