89f0f15320c08f20173b8da5de26eb380e2fb672
RYUJ1EUN <J.ryu.jieun@gmail.com>
2026-02-13 04:50:04 +0000

    Deploying to gh-pages from @ RYUJ1EUN/june.github.io@f78cfa7081c8363d4622ca47e5fabd7a0b1d8d3d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "이상을 꿈꾸고 현실을 만들어 나가는, 류지은입니다.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/june.github.io/cv/";
